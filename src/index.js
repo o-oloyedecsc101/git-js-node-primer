172c7d15961af70b5f9f572c67dd03169982303c
Oluwalonimi Oloyede <oluwalonimi.oloyede@pau.edu.ng>
2024-06-03 09:40:11 +0100

    variable declaration

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import logger, { appName, dummyFunction } from './tools.js';
+logger("Welcome! We are now having fun with modularity");
+logger("Welcome! The application name is " + appName + ". There is a functionthat returns " + dummyFunction()); 
---
+++ src/tools.js
@@ -0,0 +1,8 @@
+var logger = function (output) {
+    console.log(output)
+}
+export var appName = "ES6 Review";
+export var dummyFunction = function () {
+    return "I am a dummy function";
+}
+export default logger;
